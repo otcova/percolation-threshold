0c7c3b8d067ccc1c85f76148e1361d8dfd9e0581
XavierLoAs <xavier.lopez.asensio@gmail.com>
2024-10-03 21:03:47 +0200

    Eliminados iteradores de percolación por nodos

--- codi/percolation/percolation.cpp
+++ codi/percolation/percolation.cpp
@@ -7,40 +7,34 @@ Graph node_percolation(Graph &graph, float q) {
   std::uniform_int_distribution<std::mt19937::result_type> dist100(0, 100);
 
   int old_n_nodes = graph.number_of_nodes();
-  map<int, int> diff_accum;
+  
+  vector<int> survivors;
+  vector<bool> is_survivor(old_n_nodes,false);
+  vector<int> diff_accum(old_n_nodes,0);
   int deleted_counter = 0;
   for (int u = 0; u < old_n_nodes; ++u) {
     if (dist100(rng) / float(100) <= q)
+    {
       diff_accum[u] = deleted_counter;
-    else
-      ++deleted_counter;
+      is_survivor[u] = true;
+      survivors.push_back(u);
+    }
+    else ++deleted_counter;
   }
-  int new_n_nodes = diff_accum.size();
+  int new_n_nodes = survivors.size();
   Graph new_graph(new_n_nodes);
-  for (auto it_u = diff_accum.begin(); it_u != diff_accum.end(); ++it_u) {
-    int new_u = it_u->first - it_u->second;
-    vector<int> adjacent = graph.adjacent_nodes(
-        it_u->first); // adjacent nodes to surviving node in old graph
+  for (int u = 0; u < new_n_nodes; ++u) {
+    vector<int> adjacent = graph.adjacent_nodes(survivors[u]); // adjacent nodes to surviving node in old graph
     for (int v : adjacent) {
-      auto it_v =
-          diff_accum.find(v); // iterator points to diff_accum of v, or
-                              // diff_accum.end() if it's not a surviving node
-      if (it_v != diff_accum.end()) // v is a surviving node
+      if (is_survivor[v]) // v is a surviving node
       {
-        int new_v = v - it_v->second;
-        if (new_v > new_u)
-          new_graph.add_edge(new_u, new_v); // add edge with updated node "id"
+        int new_v = v - diff_accum[v];
+        if (new_v > u)
+          new_graph.add_edge(u, new_v); // add edge with updated node "id"
       }
     }
   }
-  /*
-  cout << "surviving nodes: " << endl;
-  for (auto it = diff_accum.begin(); it != diff_accum.end(); ++it) cout <<
-  it->first << " "; cout << endl; cout << "old graph" << endl;
-  // graph.print_graph();
-  cout << "new graph" << endl;
-  new_graph.print_graph();*/
-
+  
   return new_graph;
 }
 
