fafe069a134c8b09e8fe71967c6202a1a557d738
XinXZP <xinxiangzhou4@gmail.com>
2024-10-04 01:03:16 +0200

    1 comp conexo

--- codi/graph/generador_grafos.py
+++ codi/graph/generador_grafos.py
@@ -1,21 +1,31 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import math
 import os
 import re
 
 def generar_grafo(tipus, num_vertices, prob_radio, i):
     if tipus == "r":  #random
+        if not os.path.exists("./dades/random/original"):
+            os.makedirs("./dades/random/original")
         G = nx.erdos_renyi_graph(num_vertices, prob_radio)
         nx.write_edgelist(G, "./dades/random/original/graph"+ str(i)+".edgelist", data=False)
     if tipus == "rg":  #random geometric
-        G = nx.random_geometric_graph(num_vertices, prob_radio)
+        
+        while True:  
+            G = nx.random_geometric_graph(num_vertices, prob_radio)
+            if nx.is_connected(G):  
+                break  
         nx.write_edgelist(G, "./dades/geometric/original/graph"+ str(i)+".edgelist", data=False)
     
-    #nx.draw(G, with_labels=True, node_color="skyblue", node_size=700, font_size=10, font_color="black", font_weight="bold")
-    #plt.show()
+    nx.draw(G, with_labels=True, node_color="skyblue", node_size=700, font_size=10, font_color="black", font_weight="bold")
+    plt.show()
+   
+def calcular_radio_critico(num_vertices):
+    # Fórmula del radio crítico para garantizar conexidad
+    return math.sqrt(math.log(num_vertices) / (math.pi * num_vertices))
    
 def encontrar_max_numero(directorio, tipus):
-
     patron = re.compile(r'graph(\d+)\.edgelist')
     max_num = -1
     
@@ -61,6 +71,9 @@ def main():
         if not os.path.exists("./dades/geometric/original"):
             os.makedirs("./dades/geometric/original")
         ini = encontrar_max_numero("./dades/geometric/original", tipus)         
+        radio_critico = calcular_radio_critico(num_vertices)
+        if prob_radio < radio_critico:
+            prob_radio = radio_critico
         for i in range(ini, ini+n_gen):    
             generar_grafo(tipus, num_vertices, prob_radio, i)
     else:
