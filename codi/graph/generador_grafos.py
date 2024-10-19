c5f1bcf530488e62c968436c9658e10ce49726ed
xiangfengyepan <xiangfeng.yepan@gmail.com>
2024-10-19 17:28:26 +0200

    hola

--- codi/graph/generador_grafos.py
+++ codi/graph/generador_grafos.py
@@ -4,13 +4,18 @@ import math
 import os
 import re
 
-def generar_grafo(num_vertices, radio, i):
-    while True:  
-        G = nx.random_geometric_graph(num_vertices, radio)
-        if nx.is_connected(G):  
-            break  
-    nx.write_edgelist(G, "./dades/geometric/original/graph"+ str(i)+".edgelist", data=False)
-   
+num_file = 0
+
+def generar_grafo(num_vertices, radio):
+    global num_file
+    G = nx.random_geometric_graph(num_vertices, radio)
+
+    if not nx.is_connected(G):
+        print("error", num_vertices, radio)
+        return
+    nx.write_edgelist(G, "./dades/geometric/original/graph"+ str(num_file) + ".edgelist", data=False)
+    num_file += 1
+
 def calcular_radio_critico(num_vertices):
     # Fórmula del radio crítico para garantizar conexidad
     return math.sqrt(math.log(num_vertices) / (math.pi * num_vertices))
@@ -40,12 +45,12 @@ def read_value(value_type, prompt):
 
 def main():
     
-    num_vertices = read_value(int, "Numero vertices: ")
+    # num_vertices = read_value(int, "Numero vertices: ")
 
-    print("(", float(math.sqrt(math.log(num_vertices) / (math.pi * num_vertices))), ")")
+    # print("(", float(math.sqrt(math.log(num_vertices) / (math.pi * num_vertices))), ")")
 
-    radio = read_value(float, "Radio: ")
-    n_gen = read_value(int, "Introduce el nombre a generar: ")
+    # radio = read_value(float, "Radio: ")
+    # n_gen = read_value(int, "Introduce el nombre a generar: ")
 
     if not os.path.exists("./dades"):
         os.makedirs("./dades")
@@ -53,13 +58,18 @@ def main():
     if not os.path.exists("./dades/geometric/original"):
         os.makedirs("./dades/geometric/original")
     ini = encontrar_max_numero("./dades/geometric/original")         
-    radio_critico = calcular_radio_critico(num_vertices)
-    if radio < radio_critico:
-        radio = radio_critico
-        print("[WARN] Cambiando radio", radio, "por", radio_critico)
-    for i in range(ini, ini+n_gen):
-        generar_grafo(num_vertices, radio, i)
-            
+    # radio_critico = calcular_radio_critico(num_vertices)
+    # if radio < radio_critico:
+    #     radio = radio_critico
+    #     print("[WARN] Cambiando radio", radio, "por", radio_critico)
+   # for i in range(ini, ini+n_gen):
+    #     generar_grafo(num_vertices, radio, i)
+    for nv in [1, 10, 50, 100, 500, 1000]:
+        rc = [0.2]   
+        print(nv)
+        for r in rc:
+            generar_grafo(nv, r)
+
 if __name__ == "__main__":
     main()
 
